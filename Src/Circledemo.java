e93945e7f1c662fcfb640edffbc5df0cc7a27787
TusharGrewal <tushargrewal06@gmail.com>
2019-01-14 20:02:40 +0530

    Structure added

---
+++ Src/Circledemo.java
@@ -0,0 +1,18 @@
+class Circle{
+int r=2;
+public void displayArea()
+{
+	double a=3.14*r*r;
+	System.out.println("area is"+a);
+
+}
+
+}
+public class Circledemo{
+public static void main(String args[])
+{
+	Circle c=new Circle();
+        c.r=2;
+        c.displayArea();
+}
+}
